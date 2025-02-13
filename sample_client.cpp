9622bb23eb66a2c4c037e78ad69d833d2b3f5658
robert engels <robaho@users.noreply.github.com>
2025-02-13 11:38:15 -0600

    allow sample_client to benchmark using threads

--- sample_client.cpp
+++ sample_client.cpp
@@ -2,8 +2,10 @@
 #include <latch>
 
 #include <chrono>
+#include <memory>
 #include <stdexcept>
 #include <algorithm>
+#include <thread>
 
 #include "fix_engine.h"
 #include "msg_logon.h"
@@ -37,7 +39,7 @@ class MyClient : public Initiator<> {
     std::latch& latch;
 
    public:
-    MyClient(sockaddr_in &server,std::string symbol,DefaultSessionConfig sessionConfig,std::latch& latch,Poller* poller=nullptr) : Initiator(server, sessionConfig, poller), symbol(symbol), latch(latch) {};
+    MyClient(const sockaddr_in &server,std::string symbol,DefaultSessionConfig sessionConfig,std::latch& latch,Poller* poller=nullptr) : Initiator(server, sessionConfig, poller), symbol(symbol), latch(latch) {};
     void onConnected() override {
         std::cout << "client connected!, sending logon\n";
         Logon::build(fix);
@@ -75,29 +77,176 @@ class MyClient : public Initiator<> {
     }
 };
 
+void usage() {
+    std::cout << "usage: sample_client <hostname> ( <symbol> | -bench <count> ) [-fibers]\n";
+    exit(0);
+}
+
+void doFibers(struct sockaddr_in server, int benchCount,std::string symbol) {
+    std::cout << "using fibers\n";
+    Poller poller;
+    std::thread pollerThread([&poller]() {
+        while (true) {
+            try {
+                poller.poll();
+            } catch(const std::runtime_error& err) {
+                std::cerr << "poller error: " << err.what() << "\n";
+                return;
+            }
+        }
+    });
+
+    std::vector<MyClient*> clients;
+    int workerThreads = std::max(int(std::thread::hardware_concurrency()) / 2,1);
+
+    std::vector<std::thread> workers;
+
+    boost::fibers::use_scheduling_algorithm<boost::fibers::algo::round_robin>();
+
+    typedef boost::fibers::buffered_channel<std::string> channel_t;
+    channel_t chan{2};
+
+    // benchCount is 0 if a single symbol is being quoted
+    std::latch latch(std::max(benchCount,1));
+
+    for(int i=0;i<workerThreads;i++) {
+        workers.push_back(
+        std::thread([&chan,&server,&poller,&latch,workerThreads]() {
+            boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(workerThreads,true);
+            while(true) {
+                std::string symbol;
+                if(chan.pop(symbol)!=boost::fibers::channel_op_status::success) {
+                    return;
+                }
+                struct DefaultSessionConfig sessionConfig("CLIENT_"+symbol, config::TARGET_COMP_ID);
+                auto client = new MyClient(server,symbol,sessionConfig,latch,&poller);
+                client->connect();
+                if(client->isConnected()) {
+                    std::cout << "client connected\n";
+                    client->handle();
+                }
+            }
+        }));
+    }
+    auto reporter = boost::fibers::fiber(
+    [&latch]() {
+        while(!latch.try_wait()) {
+            auto start = std::chrono::system_clock::now();
+            long startCount = quoteCount;
+            boost::this_fiber::sleep_for(std::chrono::seconds(5));
+            auto end = std::chrono::system_clock::now();
+            long nQuotes = quoteCount-startCount;
+            auto duration =
+                std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+            std::cout << "round-trip " << nQuotes << " quotes, usec per quote "
+                      << (duration.count() / (double)(nQuotes)) << ", quotes per sec "
+                      << (int)(((nQuotes) / (duration.count() / 1000000.0))) << "\n";
+
+        }
+    });
+
+    if(!benchCount) {
+        chan.push(symbol);
+    } else {
+        for(int i=0;i<benchCount;i++) {
+            auto symbol = std::string("S")+std::to_string(i);
+            chan.push(symbol);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
+
+    while(!latch.try_wait()) {
+        boost::this_fiber::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    chan.close();
+    reporter.join();
+    
+    poller.close();
+    pollerThread.join();
+
+    for(auto& worker : workers) worker.join();
+    for(auto client : clients) delete client;
+    std::cout << "all clients disconnected\n";
+}
+
+void doThreads(struct sockaddr_in server,int benchCount,std::string symbol) {
+    int nThreads = std::max(benchCount,1);
+    std::cout << "using " << nThreads << " threads\n";
+
+    std::latch latch(nThreads);
+    auto reporter = std::thread([&latch,symbol,benchCount]() {
+        while(!latch.try_wait()) {
+            auto start = std::chrono::system_clock::now();
+            long startCount = quoteCount;
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            auto end = std::chrono::system_clock::now();
+            long nQuotes = quoteCount-startCount;
+            auto duration =
+                std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+            auto symbolStr = benchCount == 0 ? " on "+symbol : "";
+
+            std::cout << "round-trip " << nQuotes << " quotes" << symbolStr <<", usec per quote "
+                      << (duration.count() / (double)(nQuotes)) << ", quotes per sec "
+                      << (int)(((nQuotes) / (duration.count() / 1000000.0))) << "\n";
+        }
+    });
+    for(int i=0;i<nThreads;i++) {
+        std::string _symbol = benchCount==0 ? symbol : std::string("S")+std::to_string(i);
+        auto thread = std::thread([&latch,_symbol,&server]() {
+            struct DefaultSessionConfig sessionConfig("CLIENT_"+_symbol, config::TARGET_COMP_ID);
+            MyClient client(server,_symbol,sessionConfig,latch);
+            client.connect();
+            if(client.isConnected()) {
+                std::cout << "client connected\n";
+                client.handle();
+            }
+        });
+        thread.detach();
+        // delay between connection requests otherwise backlog exceeded
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    while(!latch.try_wait()) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+
+    reporter.join();
+
+    std::cout << "----------- all clients disconnected\n";
+}
+
 int main(int argc, char *argv[]) {
     struct hostent *he;
     struct sockaddr_in server;
 
     std::string symbol = "IBM";
     int benchCount = 0;
+    bool fibers = false;
 
-    if(argc > 1 && strcmp(argv[1],"-h")==0) {
-        std::cout << "usage: " << argv[0] << " [hostname] [symbol]\n";
-        exit(0);
+    if(argc < 2 || strcmp(argv[1],"-h")==0) {
+        usage();
     }
 
-    const char *hostname = "localhost";
-    if (argc > 1) {
-        hostname = argv[1];
-    }
+    int n = 1;
+    char *hostname = argv[n++];
     if(argc > 2) {
-        if(strcmp("-bench",argv[2])==0) {
-            benchCount = atoi(argv[3]);
+        if(strcmp("-bench",argv[n++])==0) {
+            benchCount = atoi(argv[n++]);
         } else {
-            symbol = argv[2];
+            symbol = argv[n++];
         }
     }
+    if(n<argc) {
+        if(strcmp("-fibers",argv[n++])==0) {
+            fibers = true;
+        } else {
+            usage();
+        }
+    }
+    if(n!=argc) usage();
 
     /* resolve hostname */
     if ((he = gethostbyname(hostname)) == NULL) {
@@ -109,109 +258,9 @@ int main(int argc, char *argv[]) {
     server.sin_port = htons(config::PORT);
     server.sin_family = AF_INET;
 
-    if(benchCount>0) {
-        Poller poller;
-        std::thread pollerThread([&poller]() {
-            while (true) {
-                try {
-                    poller.poll();
-                } catch(const std::runtime_error& err) {
-                    std::cerr << "poller error: " << err.what() << "\n";
-                    return;
-                }
-            }
-        });
-        std::vector<MyClient*> clients;
-        int workerThreads = std::max(int(std::thread::hardware_concurrency()) / 2,1);
-
-        std::vector<std::thread> workers;
-
-        boost::fibers::use_scheduling_algorithm<boost::fibers::algo::round_robin>();
-
-        typedef boost::fibers::buffered_channel<std::string> channel_t;
-        channel_t chan{2};
-
-        std::latch latch(benchCount);
-
-        for(int i=0;i<workerThreads;i++) {
-            workers.push_back(
-            std::thread([&chan,&server,&poller,&latch,workerThreads]() {
-                boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(workerThreads,true);
-                while(true) {
-                    std::string symbol;
-                    if(chan.pop(symbol)!=boost::fibers::channel_op_status::success) {
-                        return;
-                    }
-                    struct DefaultSessionConfig sessionConfig("CLIENT_"+symbol, config::TARGET_COMP_ID);
-                    auto client = new MyClient(server,symbol,sessionConfig,latch,&poller);
-                    client->connect();
-                    if(client->isConnected()) {
-                        std::cout << "client connected\n";
-                        client->handle();
-                    }
-                }
-            }));
-        }
-        auto reporter = boost::fibers::fiber(
-        [&latch]() {
-            while(!latch.try_wait()) {
-                auto start = std::chrono::system_clock::now();
-                long startCount = quoteCount;
-                boost::this_fiber::sleep_for(std::chrono::seconds(5));
-                auto end = std::chrono::system_clock::now();
-                long nQuotes = quoteCount-startCount;
-                auto duration =
-                    std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-                std::cout << "round-trip " << nQuotes << " quotes, usec per quote "
-                          << (duration.count() / (double)(nQuotes)) << ", quotes per sec "
-                          << (int)(((nQuotes) / (duration.count() / 1000000.0))) << "\n";
-
-            }
-        });
-
-        for(int i=0;i<benchCount;i++) {
-            auto symbol = std::string("S")+std::to_string(i);
-            chan.push(symbol);
-        }
-
-        while(!latch.try_wait()) {
-            boost::this_fiber::sleep_for(std::chrono::milliseconds(1000));
-        }
-
-        chan.close();
-        reporter.join();
-        
-        poller.close();
-        pollerThread.join();
-
-        for(auto& worker : workers) worker.join();
-        for(auto client : clients) delete client;
-        std::cout << "all clients disconnected\n";
+    if(fibers) {
+        doFibers(server,benchCount,symbol);
     } else {
-        std::latch latch(1);
-        auto reporter = std::thread([&latch,symbol]() {
-            while(!latch.try_wait()) {
-                auto start = std::chrono::system_clock::now();
-                long startCount = quoteCount;
-                boost::this_fiber::sleep_for(std::chrono::seconds(5));
-                auto end = std::chrono::system_clock::now();
-                long nQuotes = quoteCount-startCount;
-                auto duration =
-                    std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-                std::cout << "round-trip " << nQuotes << " quotes on " << symbol << ", usec per quote "
-                          << (duration.count() / (double)(nQuotes)) << ", quotes per sec "
-                          << (int)(((nQuotes) / (duration.count() / 1000000.0))) << "\n";
-            }
-        });
-        struct DefaultSessionConfig sessionConfig("CLIENT_"+symbol, config::TARGET_COMP_ID);
-        MyClient client(server,symbol,sessionConfig,latch);
-        client.connect();
-        if(client.isConnected()) {
-            std::cout << "client connected\n";
-            client.handle();
-        }
-        reporter.join();
+        doThreads(server,benchCount,symbol);
     }
 }
